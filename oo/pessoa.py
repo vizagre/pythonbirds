6bfb8654e642992e217aecf62bb505060e788327
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-07-01 21:09:32 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,13 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=0):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self.nome}, você tem {self.idade} anos.'
 
 if __name__ == '__main__':
     p = Pessoa()
+    p.nome="Leonardo"
+    p.idade=49
     print(p.cumprimentar())
