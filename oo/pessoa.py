505218bffc4973709e16a3cb2e2d794f4a4ea679
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-07-06 19:45:24 -0300

    Exemplo de atributos complexos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=0):
+    def __init__(self, *filhos, nome=None, idade=0):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {self.nome}, você tem {self.idade} anos.'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
-    p.nome="Leonardo"
-    p.idade=49
+    f1 = Pessoa(nome="Amanda", idade=24)
+    f2 = Pessoa(nome="Filho Virtual", idade=5)
+    p = Pessoa(f1, f2, nome="Leonardo", idade=49)
+
     print(p.cumprimentar())
+
+    for filho in p.filhos:
+        print("Nome dependente: {} - Idade: {}".format(filho.nome, filho.idade))
