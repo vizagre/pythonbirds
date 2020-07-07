f0f8a27ccfadbb6fe6dc9af0d32c2e3ba4002b3b
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-07-06 23:00:39 -0300

    Implementacao do Carro

--- oo/carro.py
+++ oo/carro.py
@@ -89,14 +89,40 @@ O   L
     'Oeste'
 """
 
-NORTE = "Norte"
-SUL = "Sul"
-LESTE = "Leste"
-OESTE = "Oeste"
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
 
 class Direcao:
     rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
-    rotacao_a_direita_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
+    rotacao_a_esquerda_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
 
     def __init__(self):
         self.valor = NORTE
@@ -106,6 +132,7 @@ class Direcao:
 
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
 
 class Motor:
     def __init__(self):
@@ -117,4 +144,3 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
-
